4514cd2dd93d02afae8d5c5eb3aca9aacc6ba3e3
Sumaiya Akter Alo (1099) <sumaiyakhanalo@gmail.com>
2025-12-01 14:37:07 +0600

    Create 123)C Program to Sort an Array using Pointers.c

---
+++ 123)C Program to Sort an Array using Pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, a[100];
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+            if(*(a+i) > *(a+j)) { int t = *(a+i); *(a+i) = *(a+j); *(a+j) = t; }
+
+    for(int i=0;i<n;i++) printf("%d ", *(a+i));
+    return 0;
+}
