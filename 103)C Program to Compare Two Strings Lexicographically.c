a34c32e24aeb20969b537cd7fcb8dc14cec62ec9
Sumaiya Akter Alo (1099) <sumaiyakhanalo@gmail.com>
2025-12-01 14:11:57 +0600

    Create 103)C Program to Compare Two Strings Lexicographically.c

---
+++ 103)C Program to Compare Two Strings Lexicographically.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[100], b[100];
+    gets(a);
+    gets(b);
+
+    int cmp = strcmp(a, b);
+    if (cmp == 0) printf("Equal");
+    else if (cmp < 0) printf("a < b");
+    else printf("a > b");
+
+    return 0;
+}
