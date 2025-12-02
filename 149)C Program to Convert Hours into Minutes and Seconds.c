ed1e6c3c2517dd2dba977da27e7ed2f16fda917d
Sumaiya Akter Alo (1099) <sumaiyakhanalo@gmail.com>
2025-12-03 00:48:05 +0600

    Create 149)C Program to Convert Hours into Minutes and Seconds.c

---
+++ 149)C Program to Convert Hours into Minutes and Seconds.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int hours;
+    scanf("%d", &hours);
+
+    int minutes = hours * 60;
+    int seconds = hours * 3600;
+
+    printf("%d hours = %d minutes = %d seconds\n", hours, minutes, seconds);
+
+    return 0;
+}
