8f375bf04254a4fa67632ac933910cc2740d9e82
Sumaiya Akter Alo (1099) <sumaiyakhanalo@gmail.com>
2025-12-02 16:13:43 +0600

    Create 139)C Program to Copy One File into another File.c

---
+++ 139)C Program to Copy One File into another File.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    FILE *src=fopen("source.txt","r"),*dest=fopen("dest.txt","w");
+    if(!src||!dest){printf("Error");return 1;}
+    int c;
+    while((c=fgetc(src))!=EOF) fputc(c,dest);
+    fclose(src); fclose(dest);
+    return 0;
+}
